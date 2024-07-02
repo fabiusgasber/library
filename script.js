1888e982c6c351ea4a8ee297e15f5fc0e477456c
Fabius Gasber <fabiusgasber@gmail.com>
2024-07-02 11:28:30 -0400

    feat: add event listeners for modal and buttons

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ const dialog = document.querySelector('dialog');
 const closeButton = document.querySelector('#cancel');
 const submitButton = document.querySelector('#submit');
 const inputs = Array.from(document.querySelectorAll('form input'));
+
+addButton.addEventListener('click', () => {
+    dialog.showModal();
+});
+
+closeButton.addEventListener('click', () => {
+    dialog.close();
+})
+
+dialog.addEventListener('close', () => {
+    inputs.forEach(input => {
+        input.value = '';
+        input.checked = false;
+    })
+})
+
+dialog.addEventListener('submit', e => {
+    e.preventDefault();
+    addBookToLibrary(inputs[0].value, inputs[1].value, inputs[2].value, inputs[3].checked);
+    displayLibrary();
+    dialog.close();
+})
