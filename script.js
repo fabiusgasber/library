084689afed7557484f65f4c4b347b0aa980eb931
Fabius Gasber <fabiusgasber@gmail.com>
2024-07-02 11:26:39 -0400

    feat: add queries to get html elements

---
+++ script.js
@@ -0,0 +1,6 @@
+const addButton = document.querySelector('dialog + button');
+const libraryDiv = document.querySelector('.library');
+const dialog = document.querySelector('dialog');
+const closeButton = document.querySelector('#cancel');
+const submitButton = document.querySelector('#submit');
+const inputs = Array.from(document.querySelectorAll('form input'));
