b0964fda9426ef65b0bb379821a68cd8b3553f50
Fabius Gasber <fabiusgasber@gmail.com>
2024-07-02 11:30:50 -0400

    feat: add book constructor

--- script.js
+++ script.js
@@ -26,3 +26,13 @@ dialog.addEventListener('submit', e => {
     displayLibrary();
     dialog.close();
 })
+
+function Book(title, author, pages, hasRead){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.hasRead = hasRead;
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${hasRead ? 'read it' : 'not read yet'}`
+    }
+}
